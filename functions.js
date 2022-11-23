aca0ba11807dc10336be13c95b7305cc2124950c
BigSev15 <palat008@umn.edu>
2022-11-22 20:34:34 -0800

    Finished add and update employee functions

--- functions.js
+++ functions.js
@@ -28,7 +28,8 @@ const init = function () {
         } else if (response.select === menuChoices[6]) {
             updateEmployee();
         } else if (response.select === menuChoices[7]) {
-            console.log('Goodbye')
+            console.clear();
+            console.log('Goodbye');
             db.end();
         }
     })
@@ -81,7 +82,6 @@ const addRole = function() {
             for (let i=0; i <res.length;i++) {
                 departments.push(res[i].name);
             };
-            console.log(departments);
             inquirer
             .prompt([
                 {
@@ -105,8 +105,8 @@ const addRole = function() {
                 for (let i =0; i< res.length; i++) {
                     if (response.dpmt === res[i].name) {
                         dpmtID = res[i].id;
-                    }
-                }
+                    };
+                };
                 
                 db.query('INSERT INTO role(title, salary, department_id) VALUES(?,?,?)',[response.role, response.salary, dpmtID], function(err, res) {
                     if (err) throw err;
@@ -119,11 +119,102 @@ const addRole = function() {
 };
 
 const addEmployee = function() {
-
+    db.query("SELECT * FROM employee JOIN role on employee.role_id = role.id", function(err, res) {
+        if (err) { throw err;
+        } else {
+            let roles = [];
+            let managerID = [];
+            for (let i=0; i< res.length; i++) {
+                if (res[i].manager_id) {
+                    managerID.push(res[i].manager_id);
+                }
+                if (!roles.includes(res[i].title)) {
+                    roles.push(res[i].title);
+                };
+            };
+            inquirer
+            .prompt([
+                {
+                    message: "What is the employee's first name?\n",
+                    type: 'input',
+                    name: 'first',
+                },
+                {
+                    message: "What is the employee's last name?\n",
+                    type: 'input',
+                    name: 'last',
+                },
+                {
+                    message: "What is the employee's role?\n",
+                    type: 'list',
+                    name: 'role',
+                    choices: roles,
+                },
+                {
+                    message: "What is their manager's ID?",
+                    type: 'list',
+                    name: 'manager',
+                    choices: managerID,
+                },
+            ]).then((response) => {
+                let jobID;
+                for(let i=0; i<res.length; i++) {
+                    if (response.role === res.role) {
+                        jobID = res[i].role_id;
+                    };
+                };
+                db.query('INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES(?,?,?,?)', [response.first, response.last, jobID, response.manager], function(err, res) {
+                    if (err) throw err;
+                    console.log(`Added ${response.first} ${response.last} to the database.\n`);
+                    init();
+                });
+            });
+        };
+    });
 };
 
 const updateEmployee = function() {
-    db.query("SELECT first_name + ' ' + last_name FROM employee")
-}
+    db.query("SELECT * FROM employee JOIN role on employee.role_id = role.id", function(err, res) {
+        if (err) { throw err;
+        } else {
+            let employees = [];
+            let roles = [];
+            for (let i=0; i<res.length; i++) {
+                employees.push(res[i].first_name);
+                if (!roles.includes(res[i].title)) {
+                    roles.push(res[i].title);
+                };
+            };
+            inquirer
+            .prompt([
+                {
+                    message: "Which employee's role do you want to update?\n",
+                    name: 'employee',
+                    type: 'list',
+                    choices: employees,
+                },
+                {
+                    message: 'Which role do you want to assign the selected employee?\n',
+                    name: 'role',
+                    type: 'list',
+                    choices: roles,
+                },
+            ]).then((response) => {
+                let jobID;
+                for (let i=0; i< res.length; i++) {
+                    if (response.role === res.role) {
+                        jobID = res[i].role_id;
+                    };
+                };
+
+                db.query('UPDATE employee SET role_id=? WHERE first_name=?', [jobID, response.employee], function(err, res) {
+                    if (err) throw err;
+                    console.log("Updated employee's role.\n");
+                    init();
+                });
+            });
+        };
+    });
+};
 
 module.exports = { init };
